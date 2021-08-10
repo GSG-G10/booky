92dc1c81ec588ddc930eb113b5698bec30b0c75b
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-08-11 01:37:46 +0300

    handle the search endpoint Relates #3

--- src/server.js
+++ src/server.js
@@ -1,25 +1,21 @@
-<<<<<<< HEAD
 const express = require('express');
+const pathFile = require('path');
+const searchFetch = require('./handler');
 
 const app = express();
-const pathFile = require('path');
 
 const PORT = process.env.PORT || 8000;
 
 app.get('/', (request, response) => {
-    response.sendFile(pathFile.join(__dirname, '../', 'public', 'index.html'));
+  response.sendFile(pathFile.join(__dirname, '../', 'public', 'index.html'));
 });
-
+app.get('/search', (req, res) => {
+  const param = req.query.q;
+  res.send(searchFetch(param));
+});
 app.use(express.static(pathFile.join(__dirname, '../', 'public')));
 
-=======
-/* eslint-disable no-console */
-const express = require('express');
-
-const app = express();
-const PORT = process.env.PORT || 8000;
-
->>>>>>> e492b90ba28305b5a4107b5471f09dae5623d53c
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`);
+  // eslint-disable-next-line no-console
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
