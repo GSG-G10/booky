c6d940708871358190206df2715f87c7c995cd34
ameera <ameeraabedalnaser2021@gmail.com>
2021-08-11 14:08:08 +0300

    creat-dom-function to show data

--- public/dom.js
+++ public/dom.js
@@ -1,17 +1,34 @@
-const createElement = (parent,class,tag) => {
-    const ele = document.createElement(tag)
+const recommendedBooksCont = document.querySelector('.recommended-books');
+
+const createElement = (parent, className, tag) => {
+    const ele = document.createElement(tag);
     parent.appendChild(ele);
-    ele.classList.add(class)
+    ele.classList.add(className);
+    return ele;
+};
 
-}
+const showBookRecommend = (data, parentOf) => {
+    console.log(data.items[0].volumeInfo);
 
-const showBookRecommend = (response) => {
-    console.log('show');
+    for (let i = 0; i < 12; i++) {
+
+        const divParent = createElement(parentOf, 'card-cont', 'div');
+        const coverImg = data.items[i].volumeInfo.imageLinks.thumbnail;
+        const title = data.items[i].volumeInfo.title;
+        const auther = data.items[i].volumeInfo.authors[0];
+        const description = data.items[i].volumeInfo.description;
+        createElement(divParent, 'img', 'div').style.backgroundImage = `url('${coverImg}')`;
+        createElement(divParent, 'title', 'div').innerHTML = title;
+        createElement(divParent, 'auther', 'div').innerHTML = auther;
+        createElement(divParent, 'description', 'div').innerHTML = description;
+
+    }
 };
 const requestApi = () => {
-  fetch('/recommend').then((response) => showBookRecommend(response));
+    fetch('/recommend').then((response) => response.json())
+        .then((data) => showBookRecommend(data, recommendedBooksCont));
 };
 
 window.onload = (e) => {
-  requestApi();
+    requestApi();
 };
--- src/server.js
+++ src/server.js
@@ -7,20 +7,20 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.get('/', (request, response) => {
-  response.sendFile(pathFile.join(__dirname, '../', 'public', 'index.html'));
+    response.sendFile(pathFile.join(__dirname, '../', 'public', 'index.html'));
 });
 app.get('/search', (req, res) => {
-  const param = req.query.q;
-  searchFetch(res,param);
+    const param = req.query.q;
+    searchFetch(res, param);
 });
 app.get('/recommend', (request, response) => {
-  // eslint-disable-next-line no-undef
-  recommendeBooks(response);
+    // eslint-disable-next-line no-undef
+    recommendeBooks(response);
 });
 
 app.use(express.static(pathFile.join(__dirname, '../', 'public')));
 
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Example app listening at http://localhost:${PORT}`);
+    // eslint-disable-next-line no-console
+    console.log(`Example app listening at http://localhost:${PORT}`);
 });
