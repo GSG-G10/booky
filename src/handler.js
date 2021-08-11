9a025ec76e09c37839392025fe5f252a38fd8fb4
ameera <ameeraabedalnaser2021@gmail.com>
2021-08-11 07:41:14 +0300

    create handeler and rout for api Related #15

---
+++ src/handler.js
@@ -0,0 +1,11 @@
+const fetch = require('node-fetch');
+
+const recommendeBooks = () => {
+    fetch('https://www.googleapis.com/books/v1/volumes?q=bookshelves')
+        .then((result) => result.json())
+        .catch((error) => {
+            console.log(error);
+        });
+};
+
+module.exports = recommendeBooks;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
 const express = require('express');
+const recommendeBooks = require('./handler')
 
 const app = express();
 const pathFile = require('path');
@@ -9,17 +9,12 @@ const PORT = process.env.PORT || 8000;
 app.get('/', (request, response) => {
     response.sendFile(pathFile.join(__dirname, '../', 'public', 'index.html'));
 });
+app.get('/recommend', (request, response) => {
+    response.end(recommendeBooks());
+});
 
 app.use(express.static(pathFile.join(__dirname, '../', 'public')));
 
-=======
-/* eslint-disable no-console */
-const express = require('express');
-
-const app = express();
-const PORT = process.env.PORT || 8000;
-
->>>>>>> e492b90ba28305b5a4107b5471f09dae5623d53c
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
 });
