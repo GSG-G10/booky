d8a128e7ef9fb53117e72b3779853a82c22a5721
ahmadfarid-059 <sabbahahmad093@gmail.com>
2021-08-11 15:21:02 +0300

    fix show books bugs and eslint problems Relates #22

--- public/dom.js
+++ public/dom.js
@@ -1,15 +1,33 @@
-// eslint-disable-next-line no-unused-vars
+const recommendedBooksCont = document.querySelector('.recommended-books');
+
 const createElement = (parent, className, tag) => {
   const ele = document.createElement(tag);
   parent.appendChild(ele);
   ele.classList.add(className);
+  return ele;
 };
 
-const showBookRecommend = () => {
-    console.log('show');
+const showBookRecommend = (data, parentOf) => {
+  const dataArr = data.items;
+  for (let i = 0; i < 12; i += 1) {
+    const dataKeys = Object.keys(dataArr[i]);
+    if (dataKeys.indexOf('volumeInfo') > 0) {
+      const divParent = createElement(parentOf, 'card-cont', 'div');
+      const coverImg = dataArr[i].volumeInfo.imageLinks.thumbnail;
+      const { title } = dataArr[i].volumeInfo;
+      const auther = dataArr[i].volumeInfo.authors;
+      const { description } = dataArr[i].volumeInfo;
+      createElement(divParent, 'img', 'img').src = coverImg;
+      createElement(divParent, 'title', 'div').textContent = title;
+      createElement(divParent, 'auther', 'div').textContent = auther;
+      createElement(divParent, 'description', 'div').textContent = description;
+    }
+  }
 };
 const requestApi = () => {
-  fetch('/recommend').then((response) => showBookRecommend(response));
+  fetch('/recommend').then((response) => response.json())
+    .then((data) => showBookRecommend(data, recommendedBooksCont))
+    .catch((err) => console.log(err.message));
 };
 
 window.onload = (e) => {
@@ -17,4 +35,4 @@ window.onload = (e) => {
 };
 document.querySelector('#button').addEventListener('click', () => {
   fetch('/search').then((res) => res.json()).then(console.log);
-})
+});
--- src/router.js
+++ src/router.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+
 const router = express.Router();
 const pathFile = require('path');
 const { searchFetch, recommendeBooks } = require('./handler');
@@ -11,4 +12,5 @@ router.get('/recommend', (request, response) => {
   recommendeBooks(response);
 });
 router.use(express.static(pathFile.join(__dirname, '../', 'public')));
+
 module.exports = router;
